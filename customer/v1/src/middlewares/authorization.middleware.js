02577e5378b0d41659b3a9cc977ff5e5bbbcafc7
Mehmet Doğru <mdogru685@gmail.com>
2023-08-02 11:55:20 +0300

    refactor: changed token service

--- customer/v1/src/middlewares/authorization.middleware.js
+++ customer/v1/src/middlewares/authorization.middleware.js
@@ -1,10 +1,10 @@
 const httpStatus = require("http-status");
-const userService = require("../services/user.service");
+const customerService = require("../services/customer.service");
 const ApiError = require("../responses/error.response");
 
 const verifyAndAuthorizationToken = (roles) => async (req, res, next) => {
   try {
-    const user = await userService.findById(req.userId);
+    const user = await customerService.findById(req.userId);
 
     const role = roles.some((item) => item.includes(user.role));
     if (role) {
